6c8a735e207bb943398fc7ff81d5e09dfd2a5d8b
Liu Taojie <liutaojie@Liu-Taojies-MBP.fios-router.home>
2021-01-26 22:05:29 -0500

    add feedback

--- src/main/java/com/demo/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/demo/DockerJenkinsIntegrationApplication.java
@@ -2,12 +2,20 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Docker!";
 	}
 
 }
